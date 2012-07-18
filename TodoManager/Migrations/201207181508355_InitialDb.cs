7239b67e7ef31080ffa47291c390473414ecdb01
Ely Lucas <ely@meta-tek.net>
2012-07-18 10:44:46 -0600

    finsished

---
+++ TodoManager/Migrations/201207181508355_InitialDb.cs
@@ -0,0 +1,27 @@
+namespace TodoManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDb : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Todoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        Done = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Todoes");
+        }
+    }
+}
---
+++ TodoManager/Scripts/app.js
@@ -0,0 +1,81 @@
+﻿var App = App || {};
+
+(function() {
+
+    App.TodoModel = function() {
+        var self = this;
+        self.Id = ko.observable(0);
+        self.Description = ko.observable('');
+        self.Done = ko.observable(false);
+        return self;
+    };
+
+    App.TodoViewModel = function() {
+        self = this;
+        self.todos = ko.observableArray([]);
+        self.tempTodo = ko.observable(new App.TodoModel());
+        self.loadData = function() {
+            self.todos([]);
+            $.ajax("/api/todo", {
+                type: "GET",
+                contentType: "appliation/json",
+                statusCode: {
+                    200: function(data) {
+                        var mappedTodos = $.map(data, function(item) {
+                            var todo = new App.TodoModel()
+                                .Id(item.Id)
+                                .Description(item.Description)
+                                .Done(item.Done);
+                            return todo;
+                        });
+                        self.todos(mappedTodos);
+                    }
+                }
+            });
+        };
+        self.addTodo = function (todo) {
+            $.ajax("/api/todo/", {
+                data: ko.toJSON(todo),
+                type: "POST",
+                contentType: "application/json",
+                statusCode: {
+                    201: function () {
+                        self.loadData();
+                        self.tempTodo(new App.TodoModel());
+                    }
+                }
+            });
+        };
+        self.saveTodo = function(todo) {
+            $.ajax("/api/todo/", {
+                data: ko.toJSON(todo),
+                type: "PUT",
+                contentType: "application/json",
+                statusCode: {
+                    200: function() {
+                        self.loadData();
+                    }
+                }
+            });
+        };
+        self.deleteTodo = function (todo) {
+            $.ajax("/api/todo/" + todo.Id(), {
+                type: "DELETE",
+                contentType: "application/json",
+                statusCode: {
+                    200: function () {
+                        self.loadData();
+                    }
+                }
+            });
+        };
+        return self;
+    };
+
+})();
+
+$(function() {
+    var todoViewModel = new App.TodoViewModel();
+    todoViewModel.loadData();
+    ko.applyBindings(todoViewModel);
+});
